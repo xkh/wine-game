58df32861c04ac67fc9f4b5d463955dc2d48e88c
xingkaihang <xingkaihang@100.me>
2022-05-14 17:27:57 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,16 @@
         },
         onLaunch() {
             console.log('App Launch')
+            const updateManager = uni.getUpdateManager();
+            updateManager.onUpdateReady(() => {
+                uni.showModal({
+                    title: "更新提示",
+                    content: "新版本已经准备好，请重启应用体验新功能",
+                    complete: function () {
+                        updateManager.applyUpdate();
+                    },
+                });
+            });
         },
         onShow() {
             console.log('App Show')
